e40910e97202ec6dc5bfc08902af9c2522909a3d
Samir Triande <habib.triande@gmail.com>
2020-04-21 01:59:20 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method her
 def valid_move?(board, index)
-  if index.between?(0,8) && !position_taken?(board, index)
+  if !position_taken?(board, index) && index.between?(0,8)
     true
   else
     false
